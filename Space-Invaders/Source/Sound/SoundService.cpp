49615df39f635ae7056046b90149c9d99687add6
AyaanRawtani <ayaanrawtani@gmail.com>
2024-06-27 13:26:28 +0530

    Declared sound service

---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,34 @@
+#include "Sound/SoundService.h"
+#include "Global/ServiceLocator.h"
+
+namespace Sound
+{
+	using namespace Global;
+
+	void SoundService::initialize()
+	{
+
+	}
+
+	void SoundService::loadBackgroundMusicFromFile()
+	{
+
+	}
+
+	void SoundService::loadSoundFromFile()
+	{
+
+	}
+
+	void SoundService::playSound(SoundType soundType)
+	{
+
+	}
+
+	void SoundService::playBackgroundMusic()
+	{
+
+	}
+
+
+}
