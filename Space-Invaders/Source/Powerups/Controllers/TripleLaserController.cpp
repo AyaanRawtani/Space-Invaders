0ec22869715ac8e11c0053bb1b467d7cc8122af4
AyaanRawtani <ayaanrawtani@gmail.com>
2024-06-28 20:26:06 +0530

    Added Powerup COntrollers

---
+++ Space-Invaders/Source/Powerups/Controllers/TripleLaserController.cpp
@@ -0,0 +1,22 @@
+#include "Powerups/Controllers/TripleLaserController.h"
+
+namespace Powerup
+{
+	namespace Controller
+	{
+		TripleLaserController::TripleLaserController(PowerupType type) : PowerupController(type)
+		{
+
+		}
+
+		TripleLaserController::~TripleLaserController()
+		{
+
+		}
+
+		void TripleLaserController::onCollected()
+		{
+
+		}
+	}
+}
---
+++ Space-Invaders/Source/Powerups/PowerupService.cpp
@@ -0,0 +1,41 @@
+#include "Powerups/PowerupService.h"
+#include "Powerups/PowerupController.h"
+#include "Collectible/ICollectible.h"
+
+namespace Powerup
+{
+	PowerupService::PowerupService()
+	{
+
+	}
+
+	PowerupService::~PowerupService()
+	{
+
+	}
+
+	void PowerupService::initialize()
+	{
+
+	}
+
+	void PowerupService::update()
+	{
+
+	}
+
+	void PowerupService::render()
+	{
+
+	}
+
+	void PowerupService::destroy()
+	{
+
+	}
+
+	void PowerupService::destroyPowerup(PowerupController* powerup_controller)
+	{
+
+	}
+}
