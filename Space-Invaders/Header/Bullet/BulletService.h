68a95a1065b2b9ab5fbfcdc8cf3320dc031e9ef7
AyaanRawtani <ayaanrawtani@gmail.com>
2024-07-06 09:57:20 +0530

    Implemented Bullet collisions

--- Space-Invaders/Header/Bullet/BulletService.h
+++ Space-Invaders/Header/Bullet/BulletService.h
@@ -17,8 +17,13 @@ namespace Bullet
 
 	private:
 		std::vector<Projectile::IProjectile*>bullet_list;
+		std::vector<Projectile::IProjectile*> flagged_bullet_list;
 
 		BulletController* createBullet(BulletType bullet_type, Entity::EntityType owner_type);
+
+		bool isValidBullet(int index_i, std::vector<Projectile::IProjectile*>& bullet_list);
+		void destroyFlaggedBullets();
+
 		void destroy();
 
 
--- Space-Invaders/Source/Bullet/BulletController.cpp
+++ Space-Invaders/Source/Bullet/BulletController.cpp
@@ -3,10 +3,18 @@
 #include "Bullet/BulletConfig.h"
 #include "Bullet/BulletView.h"
 #include "Global/ServiceLocator.h"
+#include "Player/PlayerController.h"
+#include "Element/Bunker/BunkerController.h"
+#include "Enemy/EnemyController.h"
+#include "Entity/EntityConfig.h"
 
 namespace Bullet
 {
 	using namespace Global;
+	using namespace Element::Bunker;
+	using namespace Enemy;
+	using namespace Player;
+	using namespace Entity;
 
 	BulletController::BulletController(BulletType type, Entity::EntityType owner_type)
 	{
@@ -82,7 +90,7 @@ namespace Bullet
 
 	Entity::EntityType BulletController::getOwnerEntityType()
 	{
-		return Entity::EntityType();
+		return bullet_model->getOwnerEntityType();
 	}
 
 	sf::Vector2f BulletController::getProjectilePosition()
@@ -95,5 +103,52 @@ namespace Bullet
 		return bullet_model->getBulletType();
 	}
 
-	
+	const sf::Sprite& BulletController::getColliderSprite()
+	{
+		return bullet_view->getBulletSprite();
+	}
+
+	void BulletController::onCollision(ICollider* other_collider)
+	{
+		processPlayerCollision(other_collider);
+		processEnemyCollision(other_collider);
+		processBunkerCollision(other_collider);
+		processBulletCollision(other_collider);
+	}
+
+	void BulletController::processBulletCollision(ICollider* other_collider)
+	{
+		BulletController* bullet_controller = dynamic_cast<BulletController*>(other_collider);
+
+		if (bullet_controller)
+			ServiceLocator::getInstance()->getBulletService()->destroyBullet(this);
+	}
+
+	void BulletController::processEnemyCollision(ICollider* other_collider)
+	{
+		EnemyController* enemy_controller = dynamic_cast<EnemyController*>(other_collider);
+
+		if (enemy_controller && getOwnerEntityType() != EntityType::ENEMY)
+		{
+			ServiceLocator::getInstance()->getBulletService()->destroyBullet(this);
+		}
+	}
+
+	void BulletController::processPlayerCollision(ICollider* other_collider)
+	{
+		PlayerController* player_controller = dynamic_cast<PlayerController*>(other_collider);
+
+		if (player_controller && getOwnerEntityType() != EntityType::PLAYER)
+		{
+			ServiceLocator::getInstance()->getBulletService()->destroyBullet(this);
+		}
+	}
+
+	void BulletController::processBunkerCollision(ICollider* other_collider)
+	{
+		BunkerController* bunker_controller = dynamic_cast<BunkerController*>(other_collider);
+
+		if (bunker_controller)
+			ServiceLocator::getInstance()->getBulletService()->destroyBullet(this);
+	}
 }
